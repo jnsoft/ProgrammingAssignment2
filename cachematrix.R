672b4cb71d174a225b86ade6a125c6c94cb9564b
jnsoft <jnx@live.se>
2014-07-20 02:01:22 +0200

    my third commit

--- cachematrix.R
+++ cachematrix.R
@@ -6,9 +6,7 @@
 ## a special "matrix". If the cache is empty, it calculates the inverse and stores it 
 ## in the cache before returning the inverse. 
 
-
 ## makeCacheMatrix creates a special "matrix" object
-
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -23,11 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
-
 ## cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix
 ## if the inverse has already been calculated (and the matrix has not changed), 
 ## then the cachesolve should retrieve the inverse from the cache
-
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
   if(!is.null(m)) {
